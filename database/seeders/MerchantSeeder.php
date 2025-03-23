73690f6783863fba1655213516b72c16ef36b6f0
unknown <okakzai@gmail.com>
2025-03-23 11:52:23 +0700

    Update Merchant Seeder

--- database/seeders/MerchantSeeder.php
+++ database/seeders/MerchantSeeder.php
@@ -13,10 +13,18 @@ class MerchantSeeder extends Seeder
      */
     public function run(): void
     {
-        $merchantUser = User::factory()->create([
-            'email' => 'merchant@gmail.com',
-            'password' => bcrypt('merchant@1$')
+        $merchantUser1 = User::factory()->create([
+            'email' => 'merchant1@gmail.com',
+            'password' => bcrypt('merchant1$'),
+            'saldo' => 550000
         ]);
-        $merchantUser->assignRole('merchant');
+        $merchantUser1->assignRole('merchant');
+
+        $merchantUser2 = User::factory()->create([
+            'email' => 'merchant2@gmail.com',
+            'password' => bcrypt('merchant2$'),
+            'saldo' => 250700
+        ]);
+        $merchantUser1->assignRole('merchant');
     }
 }
